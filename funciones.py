f0343b4e6621e9343ea277aaafdae64a05431f47
Janhjg <janhjanh93@gmail.com>
2026-01-16 11:27:53 +0100

    anadido test

---
+++ funciones.py
@@ -0,0 +1,70 @@
+import random
+import time
+import os
+
+# Inicio
+
+def mostrar_bienvenida():
+    pass
+
+def limpiar_pantalla():
+    pass
+
+# BASE DE DATOS JSON
+
+def cargar_datos():
+    """Lee el archivo JSON y devuelve los datos."""
+    pass
+
+def guardar_datos(datos):
+    """Escribe los datos actuales en el archivo JSON."""
+    pass
+
+# GESTIÓN DE USUARIO
+
+def registrar_jugador():
+    pass
+
+def mostrar_perfil():
+    pass
+
+def actualizar_historial():
+    pass
+
+# Juegos y logica
+
+def validar_apuesta():
+    pass
+
+def jugar_tragamonedas():
+    pass
+
+def jugar_ruleta():
+    pass
+
+def jugar_dados():
+    pass
+
+def jugar_caballos():
+    pass
+
+# Menu Principal
+
+def mostrar_menu():
+    pass
+
+def menu_principal():
+    pass
+
+# SISTEMA DE PUNTOS Y CANJES
+
+def calcular_puntos_obtenidos():
+    pass
+
+def mostrar_catalogo_canje():
+    pass
+
+def ejecutar_canje():
+    pass
+
+
---
+++ test/test_cacinhub.py
@@ -0,0 +1,42 @@
+from funciones import *
+
+# TESTS DE BASE DE DATOS
+def test_inicializar_db(self):
+    pass
+
+def test_cargar_datos(self):
+    pass
+
+def test_guardar_datos(self):
+    pass
+
+# TESTS DE GESTIÓN DE USUARIO
+def test_registrar_jugador(self):
+    pass
+
+def test_mostrar_perfil(self):
+    pass
+
+# TESTS DE SISTEMA DE PUNTOS
+def test_calcular_puntos(self):
+    pass
+
+def test_realizar_canje(self):
+    pass
+
+# TESTS DE LÓGICA DE JUEGO
+def test_validar_apuesta(self):
+    pass
+
+def test_actualizar_historial(self):
+    pass
+
+# TESTS DE JUEGOS
+def test_jugar_tragamonedas(self):
+    pass
+
+def test_jugar_ruleta(self):
+    pass
+
+def test_jugar_dados(self):
+    pass
