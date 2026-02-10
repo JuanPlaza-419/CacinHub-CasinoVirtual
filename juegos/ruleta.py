8ebf2223b93c16d8fd3c85a375ffb9a06b808f9e
danielgonzalezmotos22 <danielgonzalezmotos22@gmail.com>
2026-02-10 13:42:43 +0100

    ruleta hecha

--- juegos/ruleta.py
+++ juegos/ruleta.py
@@ -5,10 +5,8 @@ class JuegoRuleta(Juego):
     def __init__(self, usuarios, uid, gestionar_apuesta, guardar_datos):
         super().__init__("ruleta", usuarios, uid, gestionar_apuesta, guardar_datos)
 
-        # Números válidos de la ruleta
         self.numeros = list(range(0, 37))
 
-        # Pares e impares
         self.rojo = [n % 2 == 0 for n in self.numeros]
         self.negro = [n % 2 != 0 for n in self.numeros]
 
@@ -21,8 +19,8 @@ class JuegoRuleta(Juego):
 
         print("\nTipo de apuesta:")
         print("1 - Número (0 al 36)")
-        print("2 - Pares")
-        print("3 - Impares")
+        print("2 - Rojo")
+        print("3 - Negro")
 
         eleccion = input("\nElija una opción (1/2/3): ")
         numero = None
@@ -50,19 +48,23 @@ class JuegoRuleta(Juego):
         if eleccion == "1":
             if ruleta == numero:
                 gana = True
+                self.procesar_resultado(apuesta, True, 37)
+            else:
+                self.procesar_resultado(apuesta, False, 0)
+
 
         elif eleccion == "2": 
             if ruleta != 0 and ruleta % 2 == 0:
                 gana = True
+                self.procesar_resultado(apuesta, True, 2)
+            else:
+                self.procesar_resultado(apuesta, False, 0)
 
         elif eleccion == "3":
             if ruleta % 2 != 0:
                 gana = True
-
-        if gana:
-            print("¡¡Has ganado!!")
-            self.gestionar_apuesta(self.uid, apuesta * 2)
-        else:
-            print("¡¡Has perdido!!")
+                self.procesar_resultado(apuesta, True, 2)
+            else:
+                self.procesar_resultado(apuesta, False, 0)
 
         print(f"\nSaldo actual: {self.usuarios[self.uid]['fichas']} fichas.")
